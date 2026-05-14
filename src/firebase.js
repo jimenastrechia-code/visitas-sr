f24d30a4f4f24c4b04332311286aeb0f7b0eed54
jimenastrechia-code <jimena.strechia@gdn.com.uy>
2026-05-14 11:51:37 -0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAi5Uk9YVzEF6Bno-Ukyti1hO_rSbiLGWk",
+  authDomain: "visitas-sr.firebaseapp.com",
+  projectId: "visitas-sr",
+  storageBucket: "visitas-sr.firebasestorage.app",
+  messagingSenderId: "254847060930",
+  appId: "1:254847060930:web:9449269d6eaec39660a45b",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
